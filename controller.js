a684fdc3925600c65ac1088dffaf0b39dbf3a79b
marcuwynu23 <marcuwynu23@gmail.com>
2022-10-24 11:58:46 +0800

    update: add controller for api route

---
+++ controller.js
@@ -0,0 +1,60 @@
+const HOME = (req, res) => {
+	return res.send('Hello World!');
+}
+
+// route controller for regions
+const REGIONS = (req, res) => {
+	if (req.query.regCode) {
+		console.log(req.query.regCode);
+		return res.json(require("./json/refregion.json")["RECORDS"].filter((item) => item.regCode == req.query.regCode));
+	} else {
+		return res.json(require("./json/refregion.json")["RECORDS"]);
+	}
+}
+// route controller for provinces
+const PROVINCES = (req, res) => {
+	if (req.query.regCode) {
+		console.log(req.query.regCode);
+		return res.json(require("./json/refprovince.json")["RECORDS"].filter((item) => item.regCode == req.query.regCode));
+	} else if (req.query.provCode) {
+		console.log(req.query.provCode);
+		return res.json(require("./json/refprovince.json")["RECORDS"].filter((item) => item.provCode == req.query.provCode));
+	} else {
+		return res.json(require("./json/refprovince.json")["RECORDS"]);
+	}
+}
+// route controller for citymuns
+const CITYMUNS = (req, res) => {
+	if (req.query.provCode && req.query.regCode) {
+		console.log(req.query.regCode, " : ", req.query.provCode);
+		return res.json(require("./json/refcitymun.json")["RECORDS"].filter((item) => item.regDesc == req.query.regCode && item.provCode == req.query.provCode));
+	}
+}
+// route controller for barangays
+const BARANGAYS = (req, res) => {
+	if (req.query.provCode && req.query.citymunCode) {
+		console.log(req.query.citymunCode, " : ", req.query.provCode);
+		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.citymunCode == req.query.citymunCode && item.provCode == req.query.provCode));
+	}
+	if (req.query.regCode) {
+		console.log(req.query.regCode);
+		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.regCode == req.query.regCode));
+	} else if (req.query.provCode) {
+		console.log(req.query.provCode);
+		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.provCode == req.query.provCode));
+	} else if (req.query.citymunCode) {
+		console.log(req.query.citymunCode);
+		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.citymunCode == req.query.citymunCode));
+	}
+	else {
+		return res.json(require("./json/refbrgy.json")["RECORDS"]);
+	}
+}
+
+
+// export route controllers
+module.exports.HOME = HOME
+module.exports.REGIONS = REGIONS
+module.exports.PROVINCES = PROVINCES
+module.exports.CITYMUNS = CITYMUNS
+module.exports.BARANGAYS = BARANGAYS
--- index.js
+++ index.js
@@ -2,75 +2,19 @@ require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
 
-
+const controller = require('./controller');
 
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-	return res.send('Hello World!');
-});
 
-app.use("/ph/regions", (req, res) => {
-	if (req.query.regCode) {
-		console.log(req.query.regCode);
-		return res.json(require("./json/refregion.json")["RECORDS"].filter((item) => item.regCode == req.query.regCode));
-	} else {
-		return res.json(require("./json/refregion.json")["RECORDS"]);
-	}
-})
-
-app.use("/ph/provinces", (req, res) => {
-	if (req.query.regCode) {
-		console.log(req.query.regCode);
-		return res.json(require("./json/refprovince.json")["RECORDS"].filter((item) => item.regCode == req.query.regCode));
-	} else if (req.query.provCode) {
-		console.log(req.query.provCode);
-		return res.json(require("./json/refprovince.json")["RECORDS"].filter((item) => item.provCode == req.query.provCode));
-	} else {
-		return res.json(require("./json/refprovince.json")["RECORDS"]);
-	}
-})
-
-
-app.use("/ph/citymun", (req, res) => {
-	if (req.query.provCode && req.query.regCode) {
-		console.log(req.query.regCode, " : ", req.query.provCode);
-		return res.json(require("./json/refcitymun.json")["RECORDS"].filter((item) => item.regDesc == req.query.regCode && item.provCode == req.query.provCode));
-	}
-
-	if (req.query.regCode) {
-		console.log(req.query.regCode);
-		return res.json(require("./json/refcitymun.json")["RECORDS"].filter((item) => item.regDesc == req.query.regCode));
-	} else if (req.query.provCode) {
-		console.log(req.query.provCode);
-		return res.json(require("./json/refcitymun.json")["RECORDS"].filter((item) => item.provCode == req.query.provCode));
-	} else {
-		return res.json(require("./json/refcitymun.json")["RECORDS"]);
-	}
-})
-app.use("/ph/barangays", (req, res) => {
-	if (req.query.provCode && req.query.citymunCode) {
-		console.log(req.query.citymunCode, " : ", req.query.provCode);
-		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.citymunCode == req.query.citymunCode && item.provCode == req.query.provCode));
-	}
-	if (req.query.regCode) {
-		console.log(req.query.regCode);
-		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.regCode == req.query.regCode));
-	} else if (req.query.provCode) {
-		console.log(req.query.provCode);
-		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.provCode == req.query.provCode));
-	} else if (req.query.citymunCode) {
-		console.log(req.query.citymunCode);
-		return res.json(require("./json/refbrgy.json")["RECORDS"].filter((item) => item.citymunCode == req.query.citymunCode));
-	}
-	else {
-		return res.json(require("./json/refbrgy.json")["RECORDS"]);
-	}
-
-})
+app.get('/', controller.HOME);
+app.use("/api/v1/ph/regions", controller.REGIONS)
+app.use("/api/v1/ph/provinces", controller.PROVINCES)
+app.use("/api/v1/ph/citymun", controller.CITYMUNS)
+app.use("/api/v1/ph/barangays", controller.BARANGAYS)
 
 
 app.listen((process.env.PORT), process.env.ADDR, () => {
-	console.log("Server is running on port 3000");
+	console.log("Server is running on " + process.env.ADDR + ":" + process.env.PORT);
 })
