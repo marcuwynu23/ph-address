d8b02f3913f8ceb8835916de9ca3a29312848e9f
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2023-06-29 23:30:17 +0800

    fix: add bootstrap to vendor
    - add bootstrap to vendor

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-require("dotenv").config()
-const path = require("path")
-const express = require('express')
-const cors = require('cors')
-const morgan = require("morgan")
-const nunjucks = require('nunjucks')
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const nunjucks = require("nunjucks");
 
+const controller = require("./controller");
 
-const controller = require('./controller')
+const app = express();
 
+nunjucks.configure(path.resolve(__dirname, "view"), {
+  express: app,
+  autoscape: true,
+  noCache: false,
+	watch:true
+});
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
+app.use(cors());
 
-const app = express()
-
-nunjucks.configure(path.resolve(__dirname, 'view'), {
-	express: app,
-	autoscape: true,
-	noCache: false
-})
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(morgan("combined"))
-app.use(cors())
-
-
-app.get('/', controller.HOME);
-app.use("/api/v1/ph/regions", controller.REGIONS)
-app.use("/api/v1/ph/provinces", controller.PROVINCES)
-app.use("/api/v1/ph/citymuns", controller.CITYMUNS)
-app.use("/api/v1/ph/barangays", controller.BARANGAYS)
-
+app.get("/", controller.HOME);
+app.use("/api/v1/ph/regions", controller.REGIONS);
+app.use("/api/v1/ph/provinces", controller.PROVINCES);
+app.use("/api/v1/ph/citymuns", controller.CITYMUNS);
+app.use("/api/v1/ph/barangays", controller.BARANGAYS);
 
 app.listen(process.env.PORT, process.env.ADDR, () => {
-	console.log("Server is running on " + process.env.ADDR + ":" + process.env.PORT)
-})
-
+  console.log(
+    "Server is running on " + process.env.ADDR + ":" + process.env.PORT
+  );
+});
